a6dc56e3d9f39122d159279b19bdebed1a2eb98e
Mugisha Thierry <thierrymugis@gmail.com>
2020-12-14 01:58:12 +0200

    Added update form

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, SelectField, TextAreaField, SubmitField
 from wtforms.validators import Required
 
@@ -11,9 +12,18 @@ class UpdateProfile(FlaskForm):
 class PostForm(FlaskForm):
     title = StringField('Title', validators=[Required()])
     category = SelectField('Category', choices=[('Hobbies','Hobbies'),('Experiences','Experiences'),('Skills','Skills')],validators=[Required()])
-    pitch = TextAreaField('Your Pitch...', validators=[Required()])
+    post = TextAreaField('Your Post...', validators=[Required()])
+    post_pic_path = StringField('Upload image url...')
     submit = SubmitField('Save')
 
 class CommentForm(FlaskForm):
     comment = TextAreaField('Comment here',validators=[Required()])
     submit = SubmitField('Comment')
+
+class UpdatePostForm(FlaskForm):   
+    title = StringField('Title', validators=[Required()])
+    category = SelectField('Category', choices=[('Hobbies','Hobbies'),('Experiences','Experiences'),('Skills','Skills')],validators=[Required()])
+    post = TextAreaField('Your post...', validators=[Required()])
+    post_pic_path = StringField('Upload image url...')
+    submit = SubmitField('Save')
+
