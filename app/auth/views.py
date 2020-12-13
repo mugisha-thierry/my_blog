f1836a01d05fe827f368de3894af46d42f1aed28
Mugisha Thierry <thierrymugis@gmail.com>
2020-12-14 01:57:41 +0200

    Added login and register routes

--- app/auth/views.py
+++ app/auth/views.py
@@ -6,38 +6,39 @@ from . import auth
 from .. import db
 from ..email import mail_message
 
-# @auth.route('/login',methods=['GET','POST'])
-# def login():
-#     login_form = LoginForm()
-#     if login_form.validate_on_submit():
-#         user = User.query.filter_by(email = login_form.email.data).first()
-#         if user is not None and user.verify_password(login_form.password.data):
-#             login_user(user,login_form.remember.data)
-#             return redirect(request.args.get('next') or url_for('main.index'))
+@auth.route('/login',methods=['GET','POST'])
+def login():
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email = login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user,login_form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
 
-#         flash('Invalid username or Password')
+        flash('Invalid username or Password')
 
-#     title = "login"
-#     return render_template('auth/login.html',login_form = login_form,title=title)
+    title = "login"
+    return render_template('auth/login.html',login_form = login_form,title=title)
 
-# @auth.route('/register',methods = ["GET","POST"])
-# def register():
-#     form = RegistrationForm()
-#     if form.validate_on_submit():
-#         user = User(email = form.email.data, username = form.username.data,password = form.password.data)
-#         db.session.add(user)
-#         db.session.commit()
+@auth.route('/register',methods = ["GET","POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
 
-#         mail_message("Welcome to Pitch-App","email/welcome_user",user.email,user=user)
+        mail_message("Welcome to Pitch-App","email/welcome_user",user.email,user=user)
 
 
 
-#         return redirect(url_for('auth.login'))
-#         title = "New Account"
-#     return render_template('auth/register.html',registration_form = form)
+        return redirect(url_for('auth.login'))
+        title = "New Account"
+    return render_template('auth/register.html',registration_form = form)
 
-# @auth.route('/logout')
-# @login_required
-# def logout():
-#     logout_user()
-#     return redirect(url_for("main.index"))    
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash(f'You logged out of your account!', 'success')
+    return redirect(url_for("main.index"))    
