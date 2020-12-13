8478bd80ddf166aa68df5061c8dc5093ef2ffb1f
Mugisha Thierry <thierrymugis@gmail.com>
2020-12-14 01:48:13 +0200

    Added a quote url

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 
 
 class Config:
-
+    QUOTES_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://thierry:Christine@99@localhost/blog'
     
